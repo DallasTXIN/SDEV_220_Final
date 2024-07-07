4058e648de85e3f1d5b945c921424bcb7c15700c
Minh Tran <mtran17@ivytech.edu>
2024-07-07 07:20:15 -0400

    testing click

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.post_list,name='post_list')
+    path('',views.index,name='index'),
+    path('car/', views.car, name='car'),
+    path('about/',views.about, name='about')
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 #creat post_list functions, take a request as parameter
-def post_list(request):
+def index(request):
     return render(request,'blog/index.html',{})
+def car(request):
+    return render(request, 'blog/car.html', {})
+def about(request):
+    return render(request, 'blog/about.html', {})
