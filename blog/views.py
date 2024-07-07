99f09fbba950d82e2935c0b8f7a545f6cc663fda
Minh Tran <mtran17@ivytech.edu>
2024-07-07 02:22:57 -0400

    move html to templates, change html for website

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+#creat post_list functions, take a request as parameter
+def post_list(request):
+    return render(request,'blog/about.html',{})
