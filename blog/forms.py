f283be4b2c1b5cc56c690cea5aa4b89b4cfffc73
Minh Tran <mtran17@ivytech.edu>
2024-07-15 08:01:46 -0400

    parts lookup show car

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Car
+from .models import Car, Part
 
 class CarSelectForm(forms.Form):
     make = forms.ChoiceField(choices=[])
@@ -9,19 +9,26 @@ class CarSelectForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-       # Populate make choices
         makes = Car.objects.values_list('make', flat=True).distinct().order_by('make')
         self.fields['make'].choices = [('', 'Select Make')] + [(make, make) for make in makes]
-        
         # Populate year choices
         years = Car.objects.values_list('year', flat=True).distinct().order_by('-year')# descending order
         self.fields['year'].choices = [('', 'Select Year')] + [(str(year), str(year)) for year in years]
-        
         # Populate model choices
         models = Car.objects.values_list('model', flat=True).distinct().order_by('model')
-        self.fields['model'].choices = [('', 'Select Model')] + [(model, model) for model in models]
-        
-        # Populate engine choices
+        self.fields['model'].choices = [('', 'Select Model')] + [(model, model) for model in models]  
+        # Populate engine choices   
         engines = Car.objects.values_list('engine', flat=True).distinct().order_by('engine')
         self.fields['engine'].choices = [('', 'Select Engine')] + [(engine, engine) for engine in engines]
 
+class PartSelectForm(forms.Form):
+    name = forms.ChoiceField(choices=[])
+    description = forms.ChoiceField(choices=[])
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Populate name choices
+        names = Part.objects.values_list('name', flat=True).distinct().order_by('name')
+        self.fields['name'].choices = [('', 'Select Part Name')] + [(name, name) for name in names]
+        # Populate description choices
+        descriptions = Part.objects.values_list('description', flat=True).distinct().order_by('description')
+        self.fields['description'].choices = [('', 'Select Description')] + [(desc, desc) for desc in descriptions]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from .forms import CarSelectForm
+from .forms import CarSelectForm, PartSelectForm
 from .models import Car, Part
 
-# Create your views here.
-#creat post_list functions, take a request as parameter
 def index(request):
     return render(request,'blog/index.html',{})
 
@@ -11,18 +9,35 @@ def about(request):
     return render(request, 'blog/about.html', {})
 
 def parts(request):
-    form = CarSelectForm(request.GET or None)#if request empty,empty form
-    parts = None#initializes none first
-    if form.is_valid(): #https://stackoverflow.com/questions/53594745/what-is-the-use-of-cleaned-data-in-django
-        make = form.cleaned_data['make']
-        year = form.cleaned_data['year']
-        model = form.cleaned_data['model']
-        engine = form.cleaned_data['engine']
+    car_form = CarSelectForm(request.GET or None)#if request empty,empty form
+    part_form = PartSelectForm(request.GET or None)
+    car_results = None#initializes none first
+    part_results = None
+    if 'car_submit' in request.GET and car_form.is_valid(): #https://stackoverflow.com/questions/53594745/what-is-the-use-of-cleaned-data-in-django
+        make = car_form.cleaned_data['make']
+        year = car_form.cleaned_data['year']
+        model = car_form.cleaned_data['model']
+        engine = car_form.cleaned_data['engine']
         if make and year and model and engine:
             car = Car.objects.filter(make=make, year=year, model=model, engine=engine).first()
             if car:
-                parts = Part.objects.filter(car=car)
+                car_results = Part.objects.filter(car=car)
             else:
-                parts = []  # No matching car found
-
-    return render(request, 'blog/parts.html', {'form': form, 'parts': parts})
+                car_results = []  # No matching car found
+    if 'part_submit' in request.GET and part_form.is_valid():
+        name = part_form.cleaned_data['name']
+        description = part_form.cleaned_data['description']
+        if name and description:
+            part = Part.objects.filter(name=name, description=description).first()
+            if part:
+                part_results = Car.objects.filter(part=part)
+            else:
+                part_results = []
+   
+    context = {
+        'car_form': car_form,
+        'part_form': part_form,
+        'car_results': car_results,
+        'part_results': part_results,
+    }
+    return render(request, 'blog/parts.html', context)
